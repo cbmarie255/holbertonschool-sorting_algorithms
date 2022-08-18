bdb787796fc395e7728d3a99fae744e41273ce61
cbmarie255 <court.b1993@gmail.com>
2022-08-17 21:52:29 -0700

    Glad I learne this trick on the last project every check counts!

---
+++ 3-quick_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ *quick_sort - will sort a list using said method
+ *@array: pointer to the array
+ *@size: size of the array
+ *Return: void
+ */
+void quick_sort(int *array, size_t size)
+{
+	(void)array;
+	(void)size;
+}
