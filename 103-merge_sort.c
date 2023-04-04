bbb39c8099b73280cc1bf0fb5b6fb73cf191400a
cbmarie255 <court.b1993@gmail.com>
2023-04-04 11:56:31 -0500

    merge sort!

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,13 +1,84 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
+ * merge - Merges two subarrays of the given array
  *
- * Return: Always 0
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ * @left: Pointer to the left subarray
+ * @left_size: Number of elements in the left subarray
+ * @right: Pointer to the right subarray
+ * @right_size: Number of elements in the right subarray
+ */
+void merge(int *array, size_t size, int *left, size_t left_size, int *right, size_t right_size)
+{
+    int *result, *left_end, *right_end;
+    size_t i, j, k;
+
+    result = malloc(sizeof(int) * size);
+    if (!result)
+        return;
+
+    left_end = left + left_size;
+    right_end = right + right_size;
+
+    i = 0, j = 0, k = 0;
+    while (left < left_end && right < right_end)
+    {
+        if (*left <= *right)
+            result[k++] = *left++;
+        else
+            result[k++] = *right++;
+    }
+    while (left < left_end)
+        result[k++] = *left++;
+    while (right < right_end)
+        result[k++] = *right++;
+
+    for (i = 0; i < size; ++i)
+        array[i] = result[i];
+
+    free(result);
+}
+
+/**
+ * merge_sort_helper - Helper function for merge sort
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ */
+void merge_sort_helper(int *array, size_t size)
+{
+    int *left, *right;
+    size_t mid;
+
+    if (size < 2)
+        return;
+
+    mid = size / 2;
+
+    left = array;
+    right = array + mid;
+
+    merge_sort_helper(left, mid);
+    merge_sort_helper(right, size - mid);
+
+    merge(array, size, left, mid, right, size - mid);
+}
+
+/**
+ * merge_sort - Sorts an array of integers in ascending order using the Merge sort algorithm
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
  */
 void merge_sort(int *array, size_t size)
 {
-  return;
+    if (!array || size < 2)
+        return;
+
+    merge_sort_helper(array, size);
 }
+
