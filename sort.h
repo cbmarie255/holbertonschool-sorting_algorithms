37fcdd48016db7f42ac3c469a95d93ce4018fcf6
cbmarie255 <court.b1993@gmail.com>
2023-04-04 12:01:37 -0500

    forgot to push the header file

--- sort.h
+++ sort.h
@@ -25,5 +25,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void merge(int *array, size_t size, int *left, size_t left_size, int *right, size_t right_size);
+void merge_sort_helper(int *array, size_t size);
+void merge_sort(int *array, size_t size);
 
 #endif
