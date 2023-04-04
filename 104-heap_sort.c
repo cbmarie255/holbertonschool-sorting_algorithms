9400e61da78c04bb2f26495e49c9f79d9f2e2004
cbmarie255 <court.b1993@gmail.com>
2023-04-04 13:46:59 -0500

    heap sort

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -16,11 +16,13 @@ void sift_down(int *heap, size_t size, size_t idx)
 	while ((2 * root) + 1 < size)
 	{
 		size_t child = 2 * root + 1;
+		
 		if (child + 1 < size && heap[child] < heap[child + 1])
 		++child;
 		if (heap[root] < heap[child])
 		{
 			int tmp = heap[root];
+
 			heap[root] = heap[child];
 			heap[child] = tmp;
 			print_array(heap, size);
@@ -53,6 +55,7 @@ void heap_sort(int *array, size_t size)
 	for (i = size - 1; i > 0; --i)
 	{
 		int tmp = array[0];
+
 		array[0] = array[i];
 		array[i] = tmp;
 		print_array(array, size);
